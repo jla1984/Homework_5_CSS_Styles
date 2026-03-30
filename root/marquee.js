e7cf20eff2ebed20fa688d1ddf3c49e1d575a967
jla1984 <112325242+jla1984@users.noreply.github.com>
2026-03-29 18:42:01 -0700

    Delete root directory

--- root/marquee.js
+++
@@ -1,5 +0,0 @@
-function greet() {
-  var name = document.getElementById("firstname").value;
-  var greeting = "Welcome, " + name + "!";
-  document.getElementById("greeting").innerHTML = greeting;
-}
